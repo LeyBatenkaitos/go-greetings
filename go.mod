7aaccdcb3e7306b945b04c26431a26e19dc1d959
Gabriel Hernandez Rivas (NTTDATA) <gabrielhernandez.nttdata@latam.com>
2024-02-18 20:33:33 -0300

    Go commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LeyBatenkaitos/greetings
+
+go 1.22.0
---
+++ greetings.go
@@ -0,0 +1,43 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+// Devuelve un saludo a una persona (name)
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("nombre vacio")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// envia varios mensajes a varias personas
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+// envia un saludo aleatorio
+func randomFormat() string {
+	formats := []string{
+		"Hola, %v bienvenido",
+		"Que bueno verte, %v",
+		"saludo, %v encantado de conocerte",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
